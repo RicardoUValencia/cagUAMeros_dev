d5a9bf85c82b6dbf83b9cbfc15fc40fb7b7230bd
juan_ <juan_@DESKTOP-E1ISJN6>
2022-01-16 16:45:59 -0600

    H.U Finalizada (BDIG-06)

--- Biblioteca/Biblioteca/Controllers/PrestamoController.cs
+++ Biblioteca/Biblioteca/Controllers/PrestamoController.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using System.Net;
 
 namespace Biblioteca.Controllers
 {
@@ -63,8 +65,45 @@ namespace Biblioteca.Controllers
             return View(prestamos);
         }
 
-        protected void correo(object sender, EventArgs e)
+        protected void Correo_infoprestamo(UsuarioDTO usinfo, Prestamo prestamo, Libro libro)
+        //UsuarioDTO usfino para mandar correo a otros usuarios.
         {
+            //Mensaje que se mandará al alumno que este solicitando el prestamo, en la fecha el .
+            string body =
+            "<body>" +
+                "<h1> Hola"+ usinfo.Nombre +", le proporcionamos la información acerca de su prestamo efectuado el día de hoy " + DateTime.Now.ToString("dddd, dd MMMM yyyy") + "</h1>"+
+                "<table> <tr>" +
+                "<th> Título</th >  <th> Autor</th>  <th>Edición</th> <th>Fecha de Devolución</th><tr>"+
+                "<tr>"+ 
+                "<td>" + libro.Titulo + "</td>" + 
+                "<td>" + libro.Nombre_Autor + "</td>" + 
+                "<td>" + libro.Edicion + "</td>" +
+                "<td>" + prestamo.Fecha_Devolucion.ToString("dddd, dd MMMM yyyy") + "</td>" +
+                "<tr>"+
+                "</table>"+
+                "<br/><br/><span>Quedamos a su disposición a través de nuestro apartado de contacto, saludos cordiales.</span>"+
+            "</body>";
+
+            //checar para después 
+            //var pass = System.Configuration.ConfigurationManager.AppSettings["email-password"].ToString();
+            //var from = System.Configuration.ConfigurationManager.AppSettings["sender"].ToString();
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587)
+            {
+                UseDefaultCredentials = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                EnableSsl = true,
+                Credentials = new NetworkCredential("bibliotecadig.is@gmail.com", "asd?123456")
+            };
+
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("blibliotecadig.is@gmail.com", "Biblioteca Digital");
+            mail.To.Add(new MailAddress(usinfo.Email));
+            mail.Subject = "Información acerca de su prestamo";
+            mail.IsBodyHtml = true;
+            mail.Body = body;
+
+            smtp.Send(mail);
 
         }
 
@@ -88,6 +127,12 @@ namespace Biblioteca.Controllers
                     };
 
                     Libro libro = bd.Libros.Where(l => l.ID.Equals(id)).FirstOrDefault();
+                    var usuarioinfo = (from u in bd.Usuarios where u.ID == prestamo.UsuarioID
+                                              select new UsuarioDTO
+                                              {
+                                                  Email = u.Email,
+                                                  Nombre = u.Nombre
+                                              }).FirstOrDefault(); 
 
 
                     if (libro.Total_Actual > 0)
@@ -97,7 +142,8 @@ namespace Biblioteca.Controllers
                         bd.Prestamos.Add(prestamo);
                         bd.SaveChanges();
 
-                        //aquí iria la funcion 
+                        //método el correo del prestamo
+                        Correo_infoprestamo(usuarioinfo, prestamo, libro);
 
 
                         return RedirectToAction("Index", "Buscar");
