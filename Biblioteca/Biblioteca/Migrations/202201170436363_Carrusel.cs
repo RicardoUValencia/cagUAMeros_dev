a8902caab09ce495cc00d701cbc91900855d466d
Hector984 <hectoruam96@gmail.com>
2022-01-16 23:10:08 -0600

    Union de HU

---
+++ Biblioteca/Biblioteca/Migrations/202201170436363_Carrusel.cs
@@ -0,0 +1,29 @@
+﻿namespace Biblioteca.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Carrusel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Carrusel",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        ImagePath = c.String(),
+                        Nombre = c.String(),
+                        Descripcion = c.String(),
+                        Url = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Carrusel");
+        }
+    }
+}
