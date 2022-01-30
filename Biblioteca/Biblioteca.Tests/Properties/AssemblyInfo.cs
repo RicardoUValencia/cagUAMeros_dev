e786f5f4566c78fe019eefe7bcda5b87a4ea8cc5
RicardoUValencia <cbi2193011480@titlani.uam.mx>
2022-01-29 19:42:52 -0600

    Actualizacion de rama

---
+++ Biblioteca/Biblioteca.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Biblioteca.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Biblioteca.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("37da91d1-a3f9-485e-9b00-c358dfa3ebef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Biblioteca/Biblioteca.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Tests
+{
+    [TestClass]
+    public class SugerenciaControllerTests
+    {
+        [TestMethod]
+        public void SugerenciasUsers()
+        {
+            var suger = new SugerenciaControllerTests();
+            var Nombre = "JeffGutierritos";
+            var Email = "";
+            var Comentario = "";
+
+            var valid = new ValidationContext(new { nombre = Nombre, correo = Email, comentario = Comentario});
+
+            var rest = suger.GetValidationResult(Nombre, Email, Comentario, valid);
+
+            Assert.AreEqual("Los campos deben estar llenos", rest.ErrorMessages);
+
+        }
+    }
+}
